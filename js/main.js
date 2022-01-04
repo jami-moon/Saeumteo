211f38361543d715aa2de2ec9cd2df8fbd9e0808
kyukyu0312 <sherlockyu155@gmail.com>
2022-01-04 23:11:34 +0900

    ✨ Feat: compliment slick slider & tab menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
 $(document).ready(function () {
-  // 풀페이지 js
-  $("#fullpage").fullpage({
-    autoScrolling: true,
-  });
-
   // 팝업창 닫기
   $(".popup__main--close").click(function () {
     $(".popup").fadeOut();
@@ -27,5 +22,32 @@ $(document).ready(function () {
 
   $(".header__qnb").mouseleave(() => {
     $(".header__qnb ul").slideUp();
+  });
+
+  // collusion 슬라이드
+  $(".collusion__slides").slick({
+    infinite: true,
+    autoplay: false,
+    autoplaySpeed: 1000,
+    speed: 300,
+    slidesToShow: 1,
+    arrows: true,
+    appendArrows: $(".slide-arrow"),
+    prevArrow: `<button class="slide-arrow--pre" type="button">이전</button>`,
+    nextArrow: `<button class="slide-arrow--next" type="button">다음</button>`,
+    dots: true,
+    dotsClass: "slide-controller",
+    customPaging: function (slider, i) {
+      return `<button class="tab" type="button">
+        ${$(slider.$slides[i]).attr("data-dot-content")}
+        </button>`;
+    },
+  });
+
+  // service tab 메뉴
+  $(".service__slide--number li").click(function () {
+    const idx = $(this).index();
+    $(".service__item").removeClass("on");
+    $(".service__item").eq(idx).addClass("on");
   });
 });
